ddf6f1d6aaa8cde93a230ac791e37b41b7bf21a9
Hanna Svirina <26anna07@ukr.net>
2025-08-17 15:43:37 +0200

    Auto-generated commit

---
+++ chunk-X25XQKGC.js
@@ -0,0 +1,1 @@
+import{Ab as s,B as i,fb as o,gb as n,y as a}from"./chunk-EAEB62OB.js";var p=class t{constructor(r){this.http=r}baseUrl=s.places.list;getPlaces(r="en"){let e=new o().set("lang",r);return this.http.get(this.baseUrl,{params:e})}static \u0275fac=function(e){return new(e||t)(i(n))};static \u0275prov=a({token:t,factory:t.\u0275fac,providedIn:"root"})};export{p as a};
